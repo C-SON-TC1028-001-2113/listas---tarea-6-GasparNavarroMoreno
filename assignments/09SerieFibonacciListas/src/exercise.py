0d979b07c1787df900880738aa00a44c670120f0
GasparNavarroMoreno <a01252422@itesm.mx>
2021-10-08 06:33:28 +0000

    act 6 Tarea

--- assignments/09SerieFibonacciListas/src/exercise.py
+++ assignments/09SerieFibonacciListas/src/exercise.py
@@ -1,6 +1,30 @@
 def main():
-    
-    pass
+    x = -1
+    suma = 0
+    y = 1
+    z = 1
+    orden = []
+    while x <= -1 :
+        x = int(input())
+    if x == 0 :
+        print(orden)
+    elif x==1 :
+        orden.append(0)
+        print(orden)
+    elif x==2 :
+        orden.append(0)
+        orden.append(1)
+        print(orden)
+    else :
+        orden.append(0)
+        orden.append(1)
+        orden.append(1)
+        for i in range(x-3) :
+            suma = y + z
+            x = y
+            y = suma
+            orden.append(suma)
+        print(orden)
 
 if __name__=='__main__':
     main()
--- assignments/17MenoresANumero/src/exercise.py
+++ assignments/17MenoresANumero/src/exercise.py
@@ -1,9 +1,23 @@
 
-
+def MenoresAlDiez(x) :
+    Map = []
+    for a in range(len(x) ):
+        for b in range(len(x[a])) :
+            if x[a][b]<10 :
+                Map.append(x[a][b])
+    return(Map)           
 def main():
+    Lista= []
+    fila = int(input())
+    columna = int(input())
+    for a in range (fila) :
+        Lista.append([])
+        for b in range (columna) :
+            num = int(input())
+            Lista[a].append(num)
+    respuesta = MenoresAlDiez(Lista)
+    print(respuesta)
    
-
-
 
 if __name__=='__main__':
     main()
